9a96b640c8802a97d48083811984e515a8d07cbb
devanshkoolwal <dkoolwal7@gmail.com>
2020-09-04 17:33:27 +0530

    Done with front end

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var unirest = require("unirest");
 var req = unirest("POST", "https://price-history-charts.p.rapidapi.com/ProductHistory");
 
 req.query({
-	"product_url": "https://www.amazon.in/Test-Exclusive-547/dp/B078BNQ318/ref=lp_21439725031_1_1?s=electronics&ie=UTF8&qid=1598948607&sr=1-1"
+	"product_url": "https://www.flipkart.com/fault-our-stars/p/itmfc3vjrhncy9ub?pid=9780737642018&lid=LSTBOK9780737642018UFQOJJ&marketplace=FLIPKART&srno=s_1_1&otracker=AS_QueryStore_OrganicAutoSuggest_1_6_na_na_na&otracker1=AS_QueryStore_OrganicAutoSuggest_1_6_na_na_na&fm=SEARCH&iid=55209e06-692b-47f8-abc6-e8c85a5a5684.9780737642018.SEARCH&ppt=sp&ppn=sp&ssid=t7vzyserr40000001599130811443&qH=9687dcaeb62433cb"
 });
 
 req.headers({
